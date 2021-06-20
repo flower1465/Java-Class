98263966273a95d8e0416e76a0e0ae01b0c92cf6
Seung jun <poodle2405@gmail.com>
2021-06-20 16:55:42 +0900

    ADD
    day0419 learn interface

---
+++ Study/src/day0419_2/Driver.java
@@ -0,0 +1,8 @@
+package day0419_2;
+
+public class Driver {
+	
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
---
+++ Study/src/day0419_2/Vehicle.java
@@ -0,0 +1,5 @@
+package day0419_2;
+
+public interface Vehicle {
+	public void run();
+}
