52a1ef61492718f06ddfb21cc250fe849aefca25
Seung jun <poodle2405@gmail.com>
2021-06-20 16:58:05 +0900

    ADD
    learn extends

---
+++ Study/src/study/Main.java
@@ -0,0 +1,15 @@
+package study;
+
+public class Main {
+	public static void main(String[] args) {
+		//Phone phone = new Phone();
+		
+		SmartPhone smartPhone = new SmartPhone("홍길동");
+		 
+		//Phone 메소드
+		smartPhone.turnOn();
+		smartPhone.turnOff();
+		
+		smartPhone.internetSearch();
+	}
+}
