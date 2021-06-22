8706cba7233dc568af22264ad7d24031f84f6846
Seung jun <poodle2405@gmail.com>
2021-06-22 14:22:15 +0900

    [ADD]
    day0426

---
+++ Study/src/day0426_a4/Calculatable.java
@@ -0,0 +1,5 @@
+package day0426_a4;
+
+public interface Calculatable {
+	public int sum();
+}
---
+++ Study/src/day0426_a4/MainTest.java
@@ -0,0 +1,9 @@
+package day0426_a4;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		Anonymous anony = new Anonymous();
+		anony.method(0, 0);
+	}
+}
