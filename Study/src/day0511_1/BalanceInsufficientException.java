9751b376013f9ba4133600d1d471f86c0cde15a6
Seung jun <poodle2405@gmail.com>
2021-06-23 13:41:26 +0900

    [ADD]
    Learm Exception

---
+++ Study/src/day0511_1/BalanceInsufficientException.java
@@ -0,0 +1,9 @@
+package day0511_1;
+
+public class BalanceInsufficientException extends Exception{
+	
+	public BalanceInsufficientException() {}
+	public BalanceInsufficientException(String message) {
+		super(message);
+	}
+}
---
+++ Study/src/day0511_1/MainTest.java
@@ -0,0 +1,19 @@
+package day0511_1;
+
+public class MainTest {
+	public static void main(String[] args) {
+		Account ac = new Account();
+		
+		ac.deposit(10000);;
+		System.out.println("예금액: " + ac.getBalance());
+		
+		try {
+			ac.withdraw(30000);
+		} catch(BalanceInsufficientException e) {
+			String message = e.getMessage();
+			System.out.println(message);
+			System.out.println();
+			e.printStackTrace();
+		}
+	}
+}
