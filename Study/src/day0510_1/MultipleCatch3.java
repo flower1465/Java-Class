b3e2a4c3c810829e6766464174217ac91c0d2aa9
Seung jun <poodle2405@gmail.com>
2021-06-24 15:09:53 +0900

    [ADD]
    Learn MultipleCatch

---
+++ Study/src/day0510_1/MultipleCatch3.java
@@ -0,0 +1,23 @@
+package day0510_1;
+
+public class MultipleCatch3 {
+	public static void main(String[] args) {
+		try {
+			String[] s = {"일"};
+			
+			String data1 = s[0];
+			String data2 = s[1];
+			int value1 = Integer.parseInt(data1);
+			int value2 = Integer.parseInt(data2);
+			int result = value1 + value2;
+			System.out.println(data1 + "+" + data2 + "+" + "=" + result);
+		} catch(ArrayIndexOutOfBoundsException | NumberFormatException e) {
+			System.out.println("실행 매개값의 수가 부족하거나 숫자로 변환할 수 없습니다.");
+		} catch(Exception e) {
+			System.out.println("실행에 문제가 있습니다.");
+		} finally {
+			System.out.println("다시 실행하세요.");
+		}
+	
+	}
+}
