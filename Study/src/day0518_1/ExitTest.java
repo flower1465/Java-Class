778b0bfe673e4e9a6e47f7c16548ab83d60e20e8
Seung jun <poodle2405@gmail.com>
2021-06-27 22:12:51 +0900

    [ADD]
    day0518

---
+++ Study/src/day0518_1/ExitTest.java
@@ -0,0 +1,16 @@
+package day0518_1;
+
+public class ExitTest {
+
+	public static void main(String[] args) {
+		System.exit(0);
+		// JVM 강제종료
+		// 일반적으로 정상 종료일 경우 0으로 지정하고
+		// 비정상 종료일 경우 0 이외의 다른 값을 준다.
+		
+		
+		//currentTimeMillis() 와 nanoTime()는
+		//검퓨터의 시계로부터 현재 시간을 읽어서
+		// 밀리세컨드 단위와 나노세컨드 단위로 
+	}
+}
