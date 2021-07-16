cb923726ec4f0f5a65516d3b222560f63ba84939
Seung jun <poodle2405@gmail.com>
2021-07-16 09:17:31 +0900

    day0531

---
+++ Study/src/day0531_1/ArrayListExample.java
@@ -0,0 +1,39 @@
+package day0531_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Java");
+		list.add("Spring");
+		list.add("Network");
+		list.add(2, "Database");
+		list.add("OS");
+		
+		int size = list.size();
+		System.out.println("총 객체수: " +size);
+		System.out.println();
+		
+		String skill = list.get(2);
+		System.out.println("2: " + skill);
+		System.out.println();
+		
+		for(int i = 0; i<list.size(); i++) {
+			String str = list.get(i);
+			System.out.println(i + ":" + str);
+		}
+		System.out.println();
+		
+		list.remove(2);
+		list.remove(2);
+		list.remove("Spring");
+		
+		for(int i = 0; i<list.size(); i++) {
+			String str = list.get(i);
+			System.out.println(i + ":" + str);
+		}
+	}
+}
