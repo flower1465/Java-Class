8be20a41f026c2fbd902f83b5170b321fcd7603f
Seung jun <poodle2405@gmail.com>
2021-07-18 18:02:25 +0900

    day0607

---
+++ Study/src/day0607_1/Coin.java
@@ -0,0 +1,15 @@
+package day0607_1;
+
+public class Coin {
+
+	private int value;
+	
+	public Coin(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+	
+}
---
+++ Study/src/day0607_1/Message.java
@@ -0,0 +1,12 @@
+package day0607_1;
+
+public class Message {
+
+	public String command;
+	public String to;
+	
+	public Message(String command, String to) {
+		this.command = command;
+		this.to = to;
+	}
+}
